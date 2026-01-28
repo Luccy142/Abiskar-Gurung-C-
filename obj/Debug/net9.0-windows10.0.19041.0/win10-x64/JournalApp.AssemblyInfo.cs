5c44afc18576be2851c2c34a7d1ee6957377deb0
abiskargurung <gurunggautam330@gmail.com>
2026-01-28 06:22:29 +0545

    push 27

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/JournalApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/JournalApp.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9fe04f51a90562935bbc39a79e1d9b321cd71525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4c3bdf3c79f14a020f18d06107dafd5ba0c0b266")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
